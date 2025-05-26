6a040c8157210b5fdfcb126bf53540113a0fa0e7
atef <atef@localhost.localdomain>
2025-05-26 21:22:25 +0300

    add python build

---
+++ vars/run_python_build.groovy
@@ -0,0 +1,37 @@
+def call() {
+pipeline{
+    agent{
+        label "agent_1"
+    }
+    environment{
+        DOCKER_USER = credentials('dockerhub-user')
+        DOCKER_PASS = credentials('dockerhub-password')
+    }
+    stages{
+        stage("build Docker image"){
+            steps{
+                script{
+                    def dockerx = new org.iti.docker()
+                    dockerx.build("python_build", "${BUILD_NUMBER}")
+                }
+            }
+ 
+               
+            
+        }
+        stage("Push Docker image"){
+            steps{
+                script{
+                    def dockerx = new org.iti.docker()
+                    dockerx.login("${DOCKER_USER}", "${DOCKER_PASS}")
+                    dockerx.push("python_build","${DOCKER_USER}", "${BUILD_NUMBER}")
+                }
+            }
+        }
+    }
+}
+
+
+
+
+}
